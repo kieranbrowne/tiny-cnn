30af7dc573263a237722b52d9015d4d991c0bcd2
Kieran Browne <kierantbrowne@gmail.com>
2017-09-28 22:18:28 +1000

    way fewer params, reaches full accuracy after 5400 steps

--- tiny-cnn.py
+++ tiny-cnn.py
@@ -23,22 +23,22 @@ y_ = tf.placeholder(tf.float32, shape=[None, 10])
 
 x_image = tf.reshape(x, [-1, 28, 28, 1])
 
-W_conv1 = weight_variable([4, 4, 1, 16])
-b_conv1 = bias_variable([16])
+W_conv1 = weight_variable([4, 4, 1, 8]) # 128
+b_conv1 = bias_variable([8])
 
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
 h_pool1 = max_pool_2x2(h_conv1)
 
-W_conv2 = weight_variable([3, 3, 16, 5])
-b_conv2 = bias_variable([5])
+W_conv2 = weight_variable([2, 2, 8, 3]) # 160
+b_conv2 = bias_variable([3])
 
 h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
 h_pool2 = max_pool_2x2(h_conv2)
 
-W_fc1 = weight_variable([7 * 7 * 5, 256])
+W_fc1 = weight_variable([7 * 7 * 3, 256])
 b_fc1 = bias_variable([256])
 
-h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*5])
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*3])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 
 keep_prob = tf.placeholder(tf.float32)
