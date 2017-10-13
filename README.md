c4a9dc5e3766cbf6deaee8ac94041be8bf4f3fec
Kieran Browne <kierantbrowne@gmail.com>
2017-10-13 18:03:19 +1100

    gpu working baby!

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Experimentation to reduce cnn weights
 conda env create
 source activate tf-3
 ```
+
+## GPU debugging
+- Needed to exclude the old GTXs with `export CUDA_VISIBLE_DEVICES=0,1,2`
+- Needed to drop the amount of memory used by tf with:
+  ```python
+  gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+  sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options))
+  ```
+- You can kill processes which are wasting gpu space by using `nvidia-smi` to find the PIDs and then kill them with `kill -9 <PID>`
--- tiny-cnn.py
+++ tiny-cnn.py
@@ -61,7 +61,9 @@ train_step = tf.train.AdamOptimizer(1e-2).minimize(cross_entropy)
 correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-with tf.Session() as sess:
+gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=0.333)
+
+with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
     sess.run(tf.global_variables_initializer())
     for i in range(20000):
         batch = mnist.train.next_batch(100)
@@ -70,7 +72,7 @@ with tf.Session() as sess:
                 x: batch[0], y_: batch[1], keep_prob: 1.0})
             print('step %d, training accuracy %g' % (i, train_accuracy))
             print('test accuracy %g' % accuracy.eval(feed_dict={
-              x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
-        train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
+                x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
+            train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
 
